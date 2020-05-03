c7ac2dbff3e5d0c3294d168b5fa927bc50c61ec6
wel15020 <wel15020@byui.edu>
2020-05-02 18:23:05 -0700

    changed lesson1 feedback

---
+++ js/js.js
@@ -0,0 +1,4 @@
+var string = document.lastModified;
+document.getElementById("modified").textContent = `Last Updated: ${string}`;
+var date = new Date();
+document.getElementById("year").textContent = date.getFullYear();
