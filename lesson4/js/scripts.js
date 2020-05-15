77b985fa08a36904e00d7265a8d55acff7693bc6
wel15020 <wel15020@byui.edu>
2020-05-14 23:58:05 -0700

    lesson 4 medium add

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,4 +1,30 @@
+// nav hamburger 
 function toggleMenu() {
  
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+// last modified code from main page
+// var string = document.lastModified;
+// document.getElementById("modified").textContent = `${string}`;
+// var date = new Date();
+// document.getElementById("year").textContent = date.getDay();
+
+// attempt to get day of week
+// var d = new Date();
+// var n = d.getDay();
+
+// array for day of week
+
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+  
+    // var n = weekday[d.getDay()];
+    document.getElementById("footerDate").textContent = weekday[d.getDay()];
