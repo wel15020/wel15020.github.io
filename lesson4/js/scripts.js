80871de10cea5f1e10dfff921af8c3c83a4839f2
wel15020 <wel15020@byui.edu>
2020-05-15 20:14:16 -0700

    dumbed down lesson  4

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -3,28 +3,33 @@ function toggleMenu() {
  
     document.getElementById("primaryNav").classList.toggle("hide");
 }
-
-// last modified code from main page
-// var string = document.lastModified;
-// document.getElementById("modified").textContent = `${string}`;
-// var date = new Date();
-// document.getElementById("year").textContent = date.getDay();
-
-// attempt to get day of week
-// var d = new Date();
-// var n = d.getDay();
-
-// array for day of week
-
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Sunday";
-    weekday[1] = "Monday";
-    weekday[2] = "Tuesday";
-    weekday[3] = "Wednesday";
-    weekday[4] = "Thursday";
-    weekday[5] = "Friday";
-    weekday[6] = "Saturday";
-  
-    // var n = weekday[d.getDay()];
-    document.getElementById("footerDate").textContent = weekday[d.getDay()];
+// get date for footer
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    const d = new Date();
+    const dayName = days[d.getDay()];
+    const monthName = months[d.getMonth()];
+    const year = d.getFullYear();
+    const fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+    document.getElementById("footerDate").textContent = fulldate;
