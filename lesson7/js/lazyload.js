67c25e01487ba6431253bc17831b8c44d297c3d4
wel15020 <wel15020@byui.edu>
2020-06-03 22:16:35 -0700

    lazyload

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,34 @@
+
+
+function onpageload(){
+  
+    const imagesToLoad = document.querySelectorAll("[data-src]");
+  
+    const imageOptions = {
+      threshold: 1,
+      rootMargin: "0px 0px 50px 0px"
+    };
+  
+    const imageObserver = new IntersectionObserver((entries, imageObserver) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting){
+          return;
+        } else {
+          preloadImage(entry.target);
+          imageObserver.unobserve(entry.target);
+        }
+      })
+    }, imageOptions);
+  
+    imagesToLoad.forEach(image => {
+      imageObserver.observe(image);
+    });
+  }
+  
+  function preloadImage(img){
+    const src = img.getAttribute("data-src");
+    if (!src){ return;} else{
+      img.src = src;
+      img.removeAttribute('data-src');
+    }
+  }
